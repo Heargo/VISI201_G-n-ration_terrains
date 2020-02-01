c40dcada9ecbea0bac5ff628fa73370e94622a65
Hearstgo <60543512+Hearstgo@users.noreply.github.com>
2020-02-01 13:43:36 +0100

    Update diamant-carre.py
    -Retrait de numpy
    -utilisation du module Image de PIL
    -plus de problème de couleur

--- diamant-carre.py
+++ diamant-carre.py
@@ -1,8 +1,6 @@
-#import des modules. Numpy pour gérer les tableaux et random pour l'aléatoire.
-import numpy as np
+#import des modules.
 from random import randint
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
+from PIL import Image
 
 def diamant_carre(n):
     '''n est un entier naturel, il défini la taille du tableau grace a la formule suivante : (2^n)+1'''
@@ -10,12 +8,20 @@ def diamant_carre(n):
     #calcul de la taille du tableau (hauteur)
     h = (2**n)+1
     #Creation d'un tableau de 0 de coté h
-    t = np.zeros((h,h))
+    t = []
+    tempo=[]
+    for x in range(h):
+        for y in range(h):
+            tempo+=[0]
+        t+=[tempo]
+        tempo=[]
+
+    #print(t)
     #initialisation des premiers coins
-    t[0, 0] = randint(-h, h)
-    t[0, h-1] = randint(-h, h)
-    t[h-1, h-1] = randint(-h, h)
-    t[h-1, 0] = randint(-h, h)
+    t[0][0] = randint(-h, h)
+    t[0][h-1] = randint(-h, h)
+    t[h-1][h-1] = randint(-h, h)
+    t[h-1][0] = randint(-h, h)
 
     #initialisation du pas
     i = h-1
@@ -26,8 +32,8 @@ def diamant_carre(n):
         #début phase diamant
         for x in range(d,h,i):
             for y in range(d,h,i):
-                moyenne = (t[x-d,y-d] + t[x-d,y+d] + t[x+d,y+d] + t[x+d,y-d])/4
-                t[x, y] = moyenne + randint(-d, d)
+                moyenne = (t[x-d][y-d] + t[x-d][y+d] + t[x+d][y+d] + t[x+d][y-d])/4
+                t[x][y] = moyenne + randint(-d, d)
 
         decalage = 0
 
@@ -41,43 +47,39 @@ def diamant_carre(n):
                 somme = 0
                 n = 0
                 if x >= d:
-                    somme += t[x-d,y]
+                    somme += t[x-d][y]
                     n +=1
                 if x + d < h:
-                    somme += t[x+d,y]
+                    somme += t[x+d][y]
                     n +=1
                 if y >= d:
-                    somme +=t[x,y-d]
+                    somme +=t[x][y-d]
                     n +=1
                 if y + d < h:
-                    somme += t[x,y+d]
+                    somme += t[x][y+d]
                     n +=1
-                t[x, y] = somme/n + randint(-d, d)
+                t[x][y] = somme/n + randint(-d, d)
         #mise à jour du pas
         i = d
 
     #renvoie le tableau
     return t
 
-def dc_to_black_and_white(t):
+def tableau_to_img(t):
     '''converti le tableau t en valeurs entre 0 et 255'''
-    h = t.shape[0]
+    h = len(t)
+    carte_hauteur = Image.new('L', (h,h), color=0)
     #parcourt chaque case de t
     for x in range(0,h):
         for y in range(0,h):            
             #carte de hauteur
-            t[x,y]=int(((t[x,y]*127.5)/h)+127.5)
-
-    return t
+            carte_hauteur.putpixel((x,y),int(((t[x][y]*127.5)/h)+127.5))
 
 
+    carte_hauteur.save('carte_hauteur.png')
 
-n = 8
-img_dc =  diamant_carre(n)
 
-tbl_img =dc_to_black_and_white(img_dc)
 
-#print(tbl_img)
-
-plt.imshow(tbl_img)
-plt.show()
+n = 5
+tableau_diamant_carre =  diamant_carre(n)
+tableau_to_img(tableau_diamant_carre)
